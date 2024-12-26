964e4e181cce3d1932a604de656c8d5c81880185
Dinesh Kundo <60452355+Dineshkundo@users.noreply.github.com>
2024-12-26 17:02:29 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The region to deploy resources"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The zone to deploy the VM"
+  type        = string
+  default     = "us-central1-a"
+}
